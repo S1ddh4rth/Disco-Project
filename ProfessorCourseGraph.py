d1a09a6b82427e9660d8c5abca52445a6284d318
AmoghBalepur03 <abalepur@gmail.com>
2023-11-27 11:24:15 +0530

    added function for creating weight matrix

--- ProfessorCourseGraph.py
+++ ProfessorCourseGraph.py
@@ -1,1 +1,11 @@
+from ProfessorsAndCourses import *
+def Matrix(CDCList,Penalty):
+    Matrix = []
+    for cdc in CDCList:
+        for Prof in Penalty:
+            for Cdc,penalty in Prof:
+                if Cdc == cdc:
+                    Matrix.append((list([cdc,penalty])))
+    return Matrix
 
+print((Matrix(CoursesInSem(1)[0],assignPenaltyCDC(profCDC(read())))))
